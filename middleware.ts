2767febe9fd9c25a42f1808fcf9dcfc353965086
Roman F. <wavystyledev@gmail.com>
2024-09-27 20:04:21 -0400

    optimized internationalization code strategy

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { i18n, Locale } from "./i18n-config";
+import { i18n } from "./i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-  const locales: readonly Locale[] = i18n.locales;
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales.slice()
-  );
-  return matchLocale(languages, locales, i18n.defaultLocale);
+function getLocale(request: NextRequest): string {
+  const acceptLanguage = request.headers.get("accept-language");
+  if (!acceptLanguage) return i18n.defaultLocale;
+  const languages = new Negotiator({
+    headers: { "accept-language": acceptLanguage },
+  }).languages();
+  return matchLocale(languages, i18n.locales, i18n.defaultLocale);
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  if (
-    [
-      "/manifest.json",
-      "/favicon.ico",
-      // Your other files in `public`
-    ].includes(pathname)
-  )
-    return;
-
+  if (["/manifest.json", "/favicon.ico"].includes(pathname)) {
+    return NextResponse.next();
+  }
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url
-      )
-    );
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
   }
+  return NextResponse.next();
 }
 
 export const config = {
