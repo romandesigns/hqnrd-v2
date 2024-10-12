3b250201a5b87f136563b832af804272795231e5
Roman F. <wavystyledev@gmail.com>
2024-10-12 12:18:52 -0400

    improved card header

--- app/[lang]/(site)/playground/page.tsx
+++ app/[lang]/(site)/playground/page.tsx
@@ -1,9 +1,12 @@
+import { CardHeader } from "@/components/ui";
 import React from "react";
 
 export default function Playground() {
   return (
-    <div className="p-4">
-      <p id="annotation">ComponentName</p>
-    </div>
+    <main className="flex h-screen items-center justify-center border border-red-500 bg-neutral-100 p-4">
+      <div className="w-full">
+        <CardHeader y={1} p={2} title="Double Bed" highlight="# 101" />
+      </div>
+    </main>
   );
 }
--- components/ui/CardHeader/index.tsx
+++ components/ui/CardHeader/index.tsx
@@ -1,32 +1,50 @@
+import { clsx } from "@/utils/clsx";
 import React from "react";
-import { twMerge } from "tailwind-merge";
 
 export function CardHeader({
-  padding = 4,
+  p = 0,
+  y = 2,
   width = "auto",
-  component,
+  highlight,
+  title,
 }: {
+  p?: number;
+  y?: number;
   padding?: number;
-  width: string;
-  component: React.ReactNode;
+  width?: string;
+  highlight?: string;
+  title?: string;
 }) {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="h-1/4 w-full p-2">
-        <div className="relative flex after:absolute after:-bottom-1 after:left-0 after:right-0 after:h-1 after:bg-neutral-500">
-          <div className="w-[10%] rounded-tl-md bg-neutral-500" />
+        <div className="relative flex after:absolute after:-bottom-1 after:left-0 after:right-0 after:h-1 after:bg-primary">
+          <div className="w-[10%] rounded-tl-md bg-primary" />
           <div
-            className={twMerge(
-              `w-${width} rounded-bl-md rounded-br-md bg-transparent p-${padding / 2} shadow-[.8px_2.5px_0px_1.5px_#737373]`,
+            className={clsx(
+              `w-${width} rounded-bl-md rounded-br-md bg-transparent p-${p / 2} shadow-[.8px_2.5px_0px_1.5px_]`,
             )}
           >
             <span
-              className={`relative -inset-y-${padding} rounded-md bg-neutral-500 p-2 py-${padding}`}
+              className={clsx(`relative -inset-y-${y} rounded-md p-${p - 1}`)}
             >
-              {component}
+              <div
+                className={`inline-block rounded-md bg-primary p-${p} px-${p + 2}`}
+              >
+                {highlight && (
+                  <p className="block text-xs font-medium text-muted-foreground">
+                    {highlight}
+                  </p>
+                )}
+                {title && (
+                  <h2 className="text-sm font-bold uppercase text-primary-foreground">
+                    {title}
+                  </h2>
+                )}
+              </div>
             </span>
           </div>
-          <div className="flex-1 rounded-tr-md bg-neutral-500" />
+          <div className="flex-1 rounded-tr-md bg-primary" />
         </div>
       </div>
     </div>
