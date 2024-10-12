389117e3aa69ccc3a18ce09a84dfd650ad69f004
Roman F. <wavystyledev@gmail.com>
2024-10-11 21:03:44 -0400

    adjusted grid to match xd design

--- components/site/home/Surrounding/index.tsx
+++ components/site/home/Surrounding/index.tsx
@@ -25,17 +25,45 @@ export function Surrounding({
             <Heading title={title} subTitle={subTitle} />
           </Divider>
         </div>
-        <Tabs defaultValue="terraze" className="mb-28 mt-10">
-          <TabsList className="mx-auto mb-10 grid w-full max-w-lg grid-cols-2">
-            <TabsTrigger value="terraze">Terraze</TabsTrigger>
-            <TabsTrigger value="hallways">Hallways</TabsTrigger>
-          </TabsList>
-          <TabsContent value="terraze">
-            <Gallery imagesArray={data.home.hero_gallery} />
-          </TabsContent>
-          <TabsContent value="hallways">
-            <Gallery imagesArray={data.home.hero_gallery} />
-          </TabsContent>
+        <Tabs
+          defaultValue="terraze"
+          className="glass-effect mb-28 mt-10 grid grid-cols-[1fr_25%] grid-rows-1 gap-4 border p-4 max-md:grid-cols-1 max-md:grid-rows-[auto_auto]"
+        >
+          <div className="w-full rounded-md bg-background p-2 pt-0 max-md:order-2">
+            <TabsContent value="terraze">
+              <Gallery imagesArray={data.home.hero_gallery} />
+            </TabsContent>
+            <TabsContent value="hallways">
+              <Gallery imagesArray={data.home.hero_gallery} />
+            </TabsContent>
+          </div>
+          <div className="h-full max-md:order-1">
+            <TabsList className="grid w-full grid-cols-2 gap-4 md:mb-10 md:h-full md:grid-cols-1 md:p-4">
+              <TabsTrigger
+                value="terraze"
+                className="h-full gap-2 md:flex md:flex-col md:items-start md:justify-center md:p-4"
+              >
+                <h3 className="text-left text-2xl font-bold max-lg:text-xl max-md:text-sm max-md:font-medium">
+                  Terraze
+                </h3>
+                <p className="text-left text-xs max-lg:hidden">
+                  Available for our guests entertainment and private events.
+                </p>
+              </TabsTrigger>
+              <TabsTrigger
+                value="hallways"
+                className="h-full gap-2 md:flex md:flex-col md:items-start md:justify-center md:p-4"
+              >
+                <h3 className="text-left text-2xl font-bold max-lg:text-xl max-md:text-sm max-md:font-medium">
+                  Common Areas
+                </h3>
+                <p className="text-left text-xs max-lg:hidden">
+                  Offering you top best quality of cleanliness and peacefulness
+                  all around.
+                </p>
+              </TabsTrigger>
+            </TabsList>
+          </div>
         </Tabs>
         <div className="flex w-full flex-col items-center justify-center">
           <Button asChild className="w-1/4">
